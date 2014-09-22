2b0f0a1216c08903cdd20920ebd5f1149a49751f
Jikku Jose <jikkujose@gmail.com>
2014-09-22 19:31:01 +0530

    initial version of the app

--- lib/prefix_with.rb
+++ lib/prefix_with.rb
@@ -1,3 +1,13 @@
 module PrefixWith
 
+  class Prefixer
+
+    def self.output( stdin: '', prefix: '' )
+      stdin.collect do |line|
+        "#{prefix.first} '#{line.chomp}'"
+      end
+    end
+
+  end
+
 end
